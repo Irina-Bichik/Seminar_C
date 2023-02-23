98838f92e825041785dbc9f6b2dea1fd662663dc
Irina <fransik@bk.ru>
2023-02-23 23:12:41 +0300

    add Convert.ToInt32

---
+++ Seminar1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Вывод текста, текст в кавычках");
+
+// int num1 = 25; // целые числа, например -2, 25, 1500
+// int num2 = 40;
+// double num2 = 13.45; //вещественное число
+// string word = "Irina"; // обязательно двойные кавычки
+// char symbol = '%'; //один символ, цифра, буква, знак, пробел - в одинарных кавычках
+// bool check = num1>num2; //true or false
+
+// Console.WriteLine("my number are " + num1  + " and " + num2 + " and it is good!");
+// Console.WriteLine($"my number are {num1} and {num2} and it is good!");
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
